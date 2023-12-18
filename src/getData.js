f6bbe8bc917026613e0f9d90f37f50ed223410bf
urosrelicc <urosrelicc@gmail.com>
2023-12-18 17:55:47 +0100

    change api url to https

--- src/getData.js
+++ src/getData.js
@@ -3,7 +3,7 @@ const defaultQuery = 'Belgrade';
 
 // Return weather data as JSON
 async function getWeatherAsJson(query) {
-    const API = `http://api.weatherapi.com/v1/forecast.json?key=3bef9b2d9209479bb84140228231412&q=${query}&days=4&aqi=no&alerts=no`;
+    const API = `https://api.weatherapi.com/v1/forecast.json?key=3bef9b2d9209479bb84140228231412&q=${query}&days=4&aqi=no&alerts=no`;
     const response = await fetch(API);
     return await response.json();
 }
